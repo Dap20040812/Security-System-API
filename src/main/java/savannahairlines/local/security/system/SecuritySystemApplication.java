27983a12a582199699ff1c3934711192e201ec17
Diego Prado <diegoprpr@unisabana.edu.co>
2023-10-18 11:10:55 -0500

    fisrt commit

---
+++ src/main/java/savannahairlines/local/security/system/SecuritySystemApplication.java
@@ -0,0 +1,13 @@
+package savannahairlines.local.security.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/savannahairlines/local/security/system/SecuritySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package savannahairlines.local.security.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
